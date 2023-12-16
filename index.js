d8dbd1b9e1975ad08e92683520284402c74fceb5
ewkimwaki <e.w.kimwaki@gmail.com>
2023-12-16 23:01:12 +0300

    tests all passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const writeCards = (names) => {
+    return names.map((names) => `Thank you, ${names}, for the wonderful surprise gift!`);
+}
+
+const countDown = (num) => {
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
